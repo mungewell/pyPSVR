fc5a0ffa872819e5fff32b8ee0d3fdf2f1401ff9
Simon Wood <simon@mungewell.org>
2017-05-31 00:44:42 -0600

    Tweak linux script and add zip of pre-defined EDID for Windows

--- edid-hacking/hook_me_up.sh
+++ edid-hacking/hook_me_up.sh
@@ -3,16 +3,20 @@
 # Add custom mode for 1920x1080 120Hz
 xrandr --newmode "1920x1080_120"  297.00  1920 2008 2052 2200  1080 1084 1089 1125  +HSync +Vsync
 xrandr --addmode HDMI-1-3 "1920x1080_120"
-xrandr --output HDMI-1-3 --mode "1920x1080_120"
+
+# Add custom mode for 1920x1080 90Hz
+xrandr --newmode "1920x1080_90"  222.75  1920 2008 2052 2200  1080 1084 1089 1125  +HSync +Vsync
+xrandr --addmode HDMI-1-3 "1920x1080_90"
 
 #cvt -r 2560 1440 120
 xrandr --newmode "2560x1440_120"  497.76  2560 2608 2640 2720  1440 1443 1448 1525 +hsync -vsync
 xrandr --addmode HDMI-1-3 "2560x1440_120"
 
 #cvt 2560 1440 60
-xrandr --newmode "2560x1440_60"  312.45  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync
+#xrandr --newmode "2560x1440_60"  312.45  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync
+#cvt -r 2560 1440 60
+xrandr --newmode "2560x1440_60"  241.70  2560 2608 2640 2720  1440 1443 1448 1481 +hsync -vsync
 xrandr --addmode HDMI-1-3 "2560x1440_60"
 
 # Select 1920x1080 120Hz
 xrandr --output HDMI-1-3 --mode "1920x1080_120"
-#xrandr | grep '\*'
